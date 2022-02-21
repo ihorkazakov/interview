f4851fb9244a6c7fd812b0e34f623fc3da352325
Ihor_Kazakov <ihor_kazakov@epam.com>
2022-02-21 08:35:04 +0200

    Set of problems

---
+++ src/patterns/topologicalSort_graph/TopologicalSort.java
@@ -0,0 +1,67 @@
+package patterns.topologicalSort_graph;
+
+import javax.swing.*;
+import java.util.*;
+
+/**
+ * Given a directed graph, find the topological ordering of its vertices
+ *
+ * Input: Vertices=4, Edges=[3, 2], [3, 0], [2, 0], [2, 1]
+ * Output: Following are the two valid topological sorts for the given gr
+ * aph:
+ * 1) 3, 2, 0, 1
+ * 2) 3, 2, 1, 0
+ */
+public class TopologicalSort {
+    public static List<Integer> sort(int vertices, int[][] edges) {
+        List<Integer> sortedOrder = new ArrayList<>();
+        if (vertices <= 0) {
+            return sortedOrder;
+        }
+
+        HashMap<Integer, Integer> inDegree = new HashMap<>();
+        HashMap<Integer, List<Integer>> graph = new HashMap<>();
+        for (int i = 0; i < vertices; i++) {
+            inDegree.put(i, 0);
+            graph.put(i, new ArrayList<>());
+        }
+
+        for (int i = 0; i < edges.length; i++) {
+            int parent = edges[i][0];
+            int child = edges[i][1];
+            graph.get(parent).add(child);
+            inDegree.put(child, inDegree.get(child) + 1);
+        }
+
+        Queue<Integer> sources = new LinkedList<>();
+        for (Map.Entry<Integer, Integer> entry : inDegree.entrySet()) {
+            if (entry.getValue() == 0) {
+                sources.add(entry.getKey());
+            }
+        }
+
+        while (!sources.isEmpty()) {
+            int vertex  = sources.poll();
+            sortedOrder.add(vertex);
+            List<Integer> children = graph.get(vertex);
+
+            for (int child : children) {
+                inDegree.put(child, inDegree.get(child) - 1);
+                if (inDegree.get(child) == 0) {
+                    sources.add(child);
+                }
+            }
+        }
+
+        if (sortedOrder.size() != vertices) {
+            return new ArrayList<>();
+        }
+
+        return sortedOrder;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> result = TopologicalSort.sort(4, new int[][]{{3, 2}, {3, 0}, {2, 0}, {2, 1}});
+        System.out.println(result);
+    }
+}
