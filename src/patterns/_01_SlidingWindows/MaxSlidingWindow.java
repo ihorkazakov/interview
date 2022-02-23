41d6949535d4b157b0436a6633f7ab63ae99572c
Ihor_Kazakov <ihor_kazakov@epam.com>
2022-02-23 17:39:04 +0200

    maxSlidingWindow update

--- src/patterns/_01_SlidingWindows/MaxSlidingWindow.java
+++ src/patterns/_01_SlidingWindows/MaxSlidingWindow.java
@@ -1,14 +1,14 @@
 package patterns._01_SlidingWindows;
 
-import java.util.LinkedList;
+import java.util.PriorityQueue;
 
 /**
  * You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of
  * the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves
  * right by one position.
- *
+ * <p>
  * Return the max sliding window.
- *
+ * <p>
  * Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
  * Output: [3,3,5,5,6,7]
  * Explanation:
@@ -22,29 +22,29 @@ import java.util.LinkedList;
  * 1  3  -1  -3  5 [3  6  7]      7
  */
 public class MaxSlidingWindow {
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        int[] max = new int[nums.length - k + 1];
-        LinkedList<Integer> list = new LinkedList<>();
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> Integer.compare(b[0], a[0]));
+        for (int i = 0; i < k; i++) {
+            maxHeap.add(new int[]{nums[i], i});
+        }
+        int[] res = new int[nums.length - k + 1];
+        int index = 1;
+        res[0] = maxHeap.peek()[0];
 
-        if (nums.length == 0) {
-            return nums;
+        for (int i = k; i < nums.length; i++) {
+            maxHeap.add(new int[]{nums[i], i});
+            while (maxHeap.peek()[1] <= i - k) {
+                maxHeap.poll();
+            }
+
+            res[index++] = maxHeap.peek()[0];
         }
 
-        for (int i = 0; i < nums.length; i++) {
-            while (list.size() > 0 && nums[i] >= nums[list.getLast()]) {
-                list.removeLast();
-            }
-            list.addLast(i);
+        return res;
+    }
 
-            if (i - k + 1 >= 0) {
-                max[i - k + 1] = nums[list.getFirst()];
-            }
-
-            if (i - k + 1 == list.getFirst()) {
-                list.removeFirst();
-            }
-        }
-
-        return max;
+    public static void main(String[] args) {
+        int[] nums = {1, 3, -1, -3, 5, 3, 6, 7};
+        System.out.println(maxSlidingWindow(nums, 3));
     }
 }
