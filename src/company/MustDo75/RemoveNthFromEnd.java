bfb0d31946146859b3f1492db05624b8961a452f
Ihor_Kazakov <ihor_kazakov@epam.com>
2022-03-01 15:57:21 +0200

    fixed RemoveNthFromEnd

--- src/company/MustDo75/RemoveNthFromEnd.java
+++ src/company/MustDo75/RemoveNthFromEnd.java
@@ -8,24 +8,26 @@ package company.MustDo75;
  */
 public class RemoveNthFromEnd {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode dummy = new ListNode(0);
-        ListNode first = dummy;
-        ListNode second = dummy;
-
-        for (int i = 1; i <= n + 1; i++) {
-            first = first.next;
+        ListNode fast = head;
+        ListNode slow = head;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
         }
 
-        while (first != null) {
-            first = first.next;
-            second = second.next;
+        if (fast == null) {
+            head = head.next;
+            return head;
         }
 
-        second.next = second.next.next;
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
 
-        return dummy.next;
+        slow.next = slow.next.next;
+
+        return head;
     }
-
 
     public static class ListNode {
         int val;
