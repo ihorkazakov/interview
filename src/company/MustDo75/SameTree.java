0fcc098d1f584bb0829f63f74a3178f3540de206
Ihor_Kazakov <ihor_kazakov@epam.com>
2022-04-11 11:45:26 +0300

    add to MastDo75

---
+++ src/company/MustDo75/SameTree.java
@@ -0,0 +1,35 @@
+package company.MustDo75;
+
+/**
+ * Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+ * Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+ *
+ * Input: p = [1,2,3], q = [1,2,3]
+ * Output: true
+ *
+ * Input: p = [1,2], q = [1,null,2]
+ * Output: false
+ */
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
---
+++ src/company/MustDo75/ValidateBinarySearchTree.java
@@ -0,0 +1,49 @@
+package company.MustDo75;
+
+import java.util.Stack;
+
+/**
+ * Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+ *
+ * A valid BST is defined as follows:
+ *
+ * The left subtree of a node contains only nodes with keys less than the node's key.
+ * The right subtree of a node contains only nodes with keys greater than the node's key.
+ * Both the left and right subtrees must also be binary search trees.
+ *
+ * Input: root = [5,1,4,null,null,3,6]
+ * Output: false
+ * Explanation: The root node's value is 5 but its right child's value is 4.
+ */
+public class ValidateBinarySearchTree {
+    public boolean isValidBST(TreeNode root) {
+        Stack<TreeNode> stack = new Stack();
+        long leftVal = Long.MIN_VALUE;
+
+        while(!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+
+            root = stack.pop();
+            if (root.val <= leftVal) {
+                return false;
+            }
+            leftVal = root.val;
+            root = root.right;
+        }
+
+        return true;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
