86bb254585390962b42fe4d6236ca855865ec1f3
Ihor_Kazakov <ihor_kazakov@epam.com>
2022-05-27 14:38:33 +0300

    add to MastDo75

---
+++ src/company/MustDo75/CourseSchedule.java
@@ -0,0 +1,63 @@
+package company.MustDo75;
+
+import java.util.*;
+
+/**
+ * There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where
+ * prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+ *
+ * For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+ * Return true if you can finish all courses. Otherwise, return false.
+ *
+ * Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+ * Output: false
+ * Explanation: There are a total of 2 courses to take.
+ * To take course 1 you should have finished course 0,
+ * and to take course 0 you should also have finished course 1. So it is impossible.
+ */
+public class CourseSchedule {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<Integer> sorted = new ArrayList<>();
+        if (numCourses <= 0) {
+            return false;
+        }
+
+        HashMap<Integer, Integer> inDegree = new HashMap<>();
+        HashMap<Integer, List<Integer>> graph = new HashMap<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            inDegree.put(i, 0);
+            graph.put(i, new ArrayList<>());
+        }
+
+        for (int i = 0; i < prerequisites.length; i++) {
+            int parent = prerequisites[i][0];
+            int child = prerequisites[i][1];
+            graph.get(parent).add(child);
+            inDegree.put(child, inDegree.get(child) + 1);
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (Map.Entry<Integer, Integer> entry : inDegree.entrySet()) {
+            if (entry.getValue() == 0) {
+                q.add(entry.getKey());
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int vertex = q.poll();
+            sorted.add(vertex);
+            List<Integer> children = graph.get(vertex);
+
+            for (int child : children) {
+                inDegree.put(child, inDegree.get(child) - 1);
+
+                if (inDegree.get(child) == 0) {
+                    q.add(child);
+                }
+            }
+        }
+
+        return sorted.size() == numCourses;
+    }
+}
