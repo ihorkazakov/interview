8681fc0588e20debab7d1402d926e7fba9fb9c39
ihorkazakov <ihor.kazakov@gmail.com>
2022-08-25 17:14:26 +0300

    add to MastDo75

---
+++ src/company/MustDo75/RansomNote.java
@@ -0,0 +1,28 @@
+package company.MustDo75;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+ * Each letter in magazine can only be used once in ransomNote.
+ *
+ * Input: ransomNote = "aa", magazine = "aab"
+ * Output: true
+ */
+public class RansomNote {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : magazine.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (char c : ransomNote.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) - 1);
+            if (map.get(c) < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
